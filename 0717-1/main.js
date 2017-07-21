5795de2427e7095b283dac6e334642fbf526340b
root <376051341xll@mail.edu.cn>
2017-07-21 10:06:34 +0800

    a

--- 0717-1/main.js
+++ 0717-1/main.js
@@ -1,35 +1,29 @@
 'use strict';
-function suiji(){
-    let random;
-    let array=[0,1,2,3,4,5,6,7,8,9];
-    array.sort(function(){ return 0.5 - Math.random() })
-    var str= array.join();
-    random=str.substring(0,7);
-    random=random.split(',');
-    for(let i=0;i<4;i++){
-        random[i]=Number(random[i]);
+
+var array = [12, 3, 4, 6, 4, 8];
+
+var a = [];
+
+a.length = array.length;
+
+for (var i = 0; i < array.length; i++) {
+
+    let count = 0;
+
+    for (var j = 0; j < array.length; j++) {
+
+        if (array[i] > array[j] && i != j) {
+
+            count++;
+
+      }
+
     }
-    return random;
-  //  console.log(typeof random[1])
+
+    a[count] = array[i];
+
 }
 
-function Compare(random,user){
-    let word=[];
-    word.length=4;
-    word[0]=0;
-    word[2]=4;
-    word[1]='A';
-    word[3]='B';
-    for(let i in random)
-    for(let j in user) {
-        if(random[i]==user[j] && i==j){
-            word[0]++;
-        }
-        if(random[i]==user[j] && i==j){
-            word[2]--;}
-    }
-    word=word.join();
-    return word;
-}
 
-suiji();
+
+console.log(a);
